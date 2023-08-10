1fa6cf0ee548e34f4cf51e91f8e2883f64c98b07
Mario Haefs <MarioHaefs@t-online.de>
2023-08-10 17:01:20 +0200

    start to design welcome component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { ImprintComponent } from './imprint/imprint.component';
 import { DataProtectionComponent } from './data-protection/data-protection.component';
 import { MenuComponent } from './menu/menu.component';
+import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
-  { path: '', component: MenuComponent },
+  { path: '', component: AppComponent },
   { path: 'imprint', component: ImprintComponent },
   { path: 'data-protection', component: DataProtectionComponent }
 ];
