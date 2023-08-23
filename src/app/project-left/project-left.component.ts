40c2b41d6f388aa9a8bc484c450e13f1662f67c6
Mario Haefs <MarioHaefs@t-online.de>
2023-08-23 18:52:27 +0200

    dynamised project components

--- src/app/project-left/project-left.component.ts
+++ src/app/project-left/project-left.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project-left',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./project-left.component.scss']
 })
 export class ProjectLeftComponent {
-
+  @Input() imgSrc!: string;
+  @Input() title!: string;
+  @Input() technologies!: string;
+  @Input() description!: string;
 }
--- src/app/project-right/project-right.component.ts
+++ src/app/project-right/project-right.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project-right',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./project-right.component.scss']
 })
 export class ProjectRightComponent {
-
+  @Input() imgSrc!: string;
+  @Input() title!: string;
+  @Input() technologies!: string;
+  @Input() description!: string;
 }
